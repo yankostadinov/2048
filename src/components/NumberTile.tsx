fb05cd9b0741ef061120ab73fef6c2d89d36e4d8
yankostadinov <yankostadinov@gmail.com>
2022-12-08 11:51:01 +0200

    animate tiles when power increases

--- src/components/NumberTile.tsx
+++ src/components/NumberTile.tsx
@@ -1,4 +1,4 @@
-import { Box, Center } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { FC } from 'react';
 import { TileData } from '../types';
@@ -7,37 +7,35 @@ interface Props {
   tileData: TileData;
 }
 
-const NumberTile: FC<Props> = ({ tileData: { power = 1, col, row } }) => {
-  console.log('col', col, 'power', power);
-  return (
-    <motion.div
-      style={{
-        position: 'absolute',
-        zIndex: power,
-      }}
-      initial={{
-        scale: 0,
-        x: col * 75,
-        y: row * 75,
-      }}
-      animate={{
-        scale: 1,
-        x: col * 75,
-        y: row * 75,
-      }}
-      transition={{ duration: 0.3 }}
+const NumberTile: FC<Props> = ({ tileData: { power = 1, col, row } }) => (
+  <motion.div
+    style={{
+      position: 'absolute',
+      zIndex: power,
+    }}
+    initial={{
+      scale: 0,
+      x: col * 75,
+      y: row * 75,
+    }}
+    animate={{
+      scale: power > 1 ? [1, 1.5, 1] : 1,
+      x: col * 75,
+      y: row * 75,
+    }}
+    transition={{ duration: 0.3 }}
+  >
+    <Center
+      w="65px"
+      h="65px"
+      bg="gray.300"
+      borderRadius={5}
+      m="5px"
+      fontSize="20px"
     >
-      <Center
-        w="65px"
-        h="65px"
-        bg="gray.300"
-        borderRadius={5}
-        m="5px"
-        fontSize="20px"
-      >
-        {Math.pow(2, power)}
-      </Center>
-    </motion.div>
-  );
-};
+      {Math.pow(2, power)}
+    </Center>
+  </motion.div>
+);
+
 export default NumberTile;

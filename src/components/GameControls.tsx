4d51e1a8264773713ef27676fad560a2bb0e4f70
yankostadinov <yankostadinov@gmail.com>
2022-12-08 12:40:31 +0200

    add keyboard shortcuts

--- src/components/GameControls.tsx
+++ src/components/GameControls.tsx
@@ -1,10 +1,24 @@
 import { Button, HStack } from '@chakra-ui/react';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { MoveDirection } from '../types';
+import config from '../utils/config';
 import useGameStore from '../utils/store';
 
 const GameControls: FC = () => {
   const move = useGameStore(store => store.move);
+
+  useEffect(() => {
+    if (!config.keyboardShortcutsEnabled) return;
+
+    const keydownListener = (event: KeyboardEvent) => {
+      if (event.key === 'ArrowLeft') move(MoveDirection.LEFT);
+      else if (event.key === 'ArrowUp') move(MoveDirection.UP);
+      else if (event.key === 'ArrowDown') move(MoveDirection.DOWN);
+      else if (event.key === 'ArrowRight') move(MoveDirection.RIGHT);
+    };
+    document.addEventListener('keydown', keydownListener);
+    return () => document.removeEventListener('keydown', keydownListener);
+  }, []);
 
   return (
     <HStack justify="center">
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,13 +1,19 @@
 interface GameConfig {
+  /** The square grid size. Size 6 creates a grid with 6 rows and 6 columns for a total of 36 tiles.*/
   size: number;
+  /** Number of obstacles placed on the grid to increase difficulty by blocking the movement of tiles. */
   obstacles: number;
+  /** The power of 2 that you must reach to win the game. A targetPower of 11 means you need to create a tile of value 2048.*/
   targetPower: number;
+  /** Disables keyboard arrow shortcuts to move the tiles. */
+  keyboardShortcutsEnabled: boolean;
 }
 
 const config: GameConfig = {
   size: 4,
   obstacles: 0,
   targetPower: 3,
+  keyboardShortcutsEnabled: true,
 };
 
 export default config;
